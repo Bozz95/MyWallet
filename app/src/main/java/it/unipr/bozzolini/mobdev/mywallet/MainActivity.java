d0293cbd231073ebc46a70c2bd73333f01ab95c2
Bozz95 <39773886+Bozz95@users.noreply.github.com>
2018-06-16 19:40:23 +0200

    database created activity add started

--- app/src/main/java/it/unipr/bozzolini/mobdev/mywallet/MainActivity.java
+++ app/src/main/java/it/unipr/bozzolini/mobdev/mywallet/MainActivity.java
@@ -1,5 +1,6 @@
 package it.unipr.bozzolini.mobdev.mywallet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,9 +11,14 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener myAddListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            
+            Intent intent = new Intent(getApplicationContext(), AddActivity.class);
+            startActivity(intent);
         }
     };
+
+    /*public void startAddActivity(View view){
+        Intent intent = new Intent(this, AddActivity.class);
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
