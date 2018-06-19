df7905fe9ffe0df3336e40fd600b95a664339aab
Bozz95 <39773886+Bozz95@users.noreply.github.com>
2018-06-19 13:54:07 +0200

    calendar and price edittext fixed

--- app/src/main/java/it/unipr/bozzolini/mobdev/mywallet/AddActivity.java
+++ app/src/main/java/it/unipr/bozzolini/mobdev/mywallet/AddActivity.java
@@ -1,5 +1,6 @@
 package it.unipr.bozzolini.mobdev.mywallet;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -8,22 +9,86 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextWatcher;
+import android.text.method.DigitsKeyListener;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+import java.util.StringTokenizer;
 
 public class AddActivity extends AppCompatActivity {
     private  static final String TAG_ADD = "TAG ADD ACTIVITY";
 
     private DbReaderDbHelper dbHelper;
 
+    private Calendar myCalendar = Calendar.getInstance();
+
+    private void updateLabel() {
+        EditText myDateText = findViewById(R.id.editTextDate);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.ITALY);
+        myDateText.setText(sdf.format(myCalendar.getTime()));
+
+    }
+
+    /**
+     * Get decimal formated string to include comma seperator to decimal number
+     *
+     * @param value
+     * @return
+     */
+    public  String getDecimalFormattedString(String value) {
+        if (value != null && !value.equalsIgnoreCase("")) {
+            StringTokenizer lst = new StringTokenizer(value, ".");
+            String str1 = value;
+            String str2 = "";
+            if (lst.countTokens() > 1) {
+                str1 = lst.nextToken();
+                str2 = lst.nextToken();
+            }
+            String str3 = "";
+            int i = 0;
+            int j = -1 + str1.length();
+            if (str1.charAt(-1 + str1.length()) == '.') {
+                j--;
+                str3 = ".";
+            }
+            for (int k = j; ; k--) {
+                if (k < 0) {
+                    if (str2.length() > 0)
+                        str3 = str3 + "." + str2;
+                    return str3;
+                }
+                if (i == 3) {
+                    str3 = "," + str3;
+                    i = 0;
+                }
+                str3 = str1.charAt(k) + str3;
+                i++;
+            }
+        }
+        return "";
+    }
+
+    // SPINNER ADAPTER
     private class MySpinnerAdapter extends ArrayAdapter<String> {
         public MySpinnerAdapter(Context context, int resource, List<String> objects) {
             super(context, resource, objects);
@@ -62,6 +127,66 @@ public class AddActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Restrict digits after decimal point value as per currency
+     */
+    class MoneyValueFilter extends DigitsKeyListener {
+        private int digits;
+
+        public MoneyValueFilter(int i) {
+            super(false, true);
+            digits = i;
+        }
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+            CharSequence out = super.filter(source, start, end, dest, dstart, dend);
+
+            // if changed, replace the source
+            if (out != null) {
+                source = out;
+                start = 0;
+                end = out.length();
+            }
+
+            int len = end - start;
+
+            // if deleting, source is empty
+            // and deleting can't break anything
+            if (len == 0) {
+                return source;
+            }
+
+            int dlen = dest.length();
+
+            // Find the position of the decimal .
+            for (int i = 0; i < dstart; i++) {
+                if (dest.charAt(i) == '.') {
+                    // being here means, that a number has
+                    // been inserted after the dot
+                    // check if the amount of digits is right
+                    return getDecimalFormattedString((dlen - (i + 1) + len > digits) ? "" : String.valueOf(new SpannableStringBuilder(source, start, end)));
+                }
+            }
+
+            for (int i = start; i < end; ++i) {
+                if (source.charAt(i) == '.') {
+                    // being here means, dot has been inserted
+                    // check if the amount of digits is right
+                    if ((dlen - dend) + (end - (i + 1)) > digits)
+                        return "";
+                    else
+                        break; // return new SpannableStringBuilder(source,
+                    // start, end);
+                }
+            }
+
+            // if the dot is after the inserted part,
+            // nothing can break
+            return getDecimalFormattedString(String.valueOf(new SpannableStringBuilder(source, start, end)));
+        }
+    }
+
     private void setupSpinner(Spinner spinner){
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
@@ -93,17 +218,6 @@ public class AddActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
     }
 
-    private View.OnClickListener startAddCategory = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Log.d(TAG_ADD, "provo a creare l'intent");
-            Intent intent = new Intent(AddActivity.this, AddCategory.class);
-            Log.d(TAG_ADD, "About to start new activity add category");
-            startActivityForResult(intent, 1);
-            //startActivity(intent);
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -111,14 +225,117 @@ public class AddActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_add);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.myToolbar);
-        setSupportActionBar(myToolbar);
+        EditText date = findViewById(R.id.editTextDate);
+        date.setOnClickListener(popupCalendar);
+
+        EditText etAmount = findViewById(R.id.editTextAmount);
+        etAmount.addTextChangedListener(amountTextWatcher);
 
         Spinner spinner = findViewById(R.id.spinner);
         setupSpinner(spinner);
 
         Button btnStartAddCategory = findViewById(R.id.button_add_category);
         btnStartAddCategory.setOnClickListener(startAddCategory);
+    }
+
+    /// EVENT LISTENERS
+    private View.OnClickListener startAddCategory = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG_ADD, "provo a creare l'intent");
+            Intent intent = new Intent(AddActivity.this, AddCategory.class);
+            Log.d(TAG_ADD, "About to start new activity add category");
+            startActivityForResult(intent, 1);
+        }
+    };
+
+    private View.OnClickListener popupCalendar = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            new DatePickerDialog(AddActivity.this, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        }
+    };
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    TextWatcher amountTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            EditText etAmount = findViewById(R.id.editTextAmount);
+            int cursorPosition = etAmount.getSelectionEnd();
+            String originalStr = etAmount.getText().toString();
+
+            //To restrict only two digits after decimal place
+            etAmount.setFilters(new InputFilter[]{new MoneyValueFilter(2)});
+
+            try {
+                etAmount.removeTextChangedListener(this);
+                String value = etAmount.getText().toString();
+
+                if (value != null && !value.equals("")) {
+                    if (value.startsWith(".")) {
+                        etAmount.setText("0.");
+                    }
+                    if (value.startsWith("0") && !value.startsWith("0.")) {
+                        etAmount.setText("");
+                    }
+                    String str = etAmount.getText().toString().replaceAll(",", "");
+                    if (!value.equals(""))
+                        etAmount.setText(getDecimalFormattedString(str));
+
+                    int diff = etAmount.getText().toString().length() - originalStr.length();
+                    etAmount.setSelection(cursorPosition + diff);
+                }
+                etAmount.addTextChangedListener(this);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                etAmount.addTextChangedListener(this);
+            }
+        }
+    };
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                //TODO
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     @Override
